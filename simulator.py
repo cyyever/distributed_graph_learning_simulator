8430f56eab43773bd8e5e0cd377eb100e5b9a49d
cyy <cyyever@outlook.com>
2024-11-14 12:54:57 +0800

    Fix deps

--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 import os
 
 import hydra
+import cyy_torch_graph  # noqa: F401
 from distributed_learning_simulation.config import DistributedTrainingConfig
 from distributed_learning_simulation.config import load_config as __load_config
 from distributed_learning_simulation.training import train
