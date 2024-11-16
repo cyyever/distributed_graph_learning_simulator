e9538aa92ddb02771973726b2cccebd432da24fc
cyy <cyyever@outlook.com>
2024-11-16 19:08:21 +0800

    Fix code

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 # Graph
 python3 ./simulator.py --config-name fed_gnn/cs.yaml ++fed_gnn.round=1 ++fed_gnn.epoch=1 ++fed_gnn.worker_number=2 ++fed_gnn.debug=False
+python3 ./simulator.py --config-name fed_aas/yelp.yaml ++fed_aas.round=1 ++fed_aas.epoch=1 ++fed_aas.worker_number=2 ++fed_aas.debug=False
