5f4adffeba1ee5f38e0f7af26b147018118195a6
cyy <cyyever@outlook.com>
2025-02-27 10:09:01 +0800

    Fix code

--- simulator.py
+++ simulator.py
@@ -1,25 +1,16 @@
 import os
 
-import hydra
-import method  # noqa: F401
-from distributed_learning_simulation.config import DistributedTrainingConfig
-from distributed_learning_simulation.config import load_config as __load_config
-from distributed_learning_simulation.training import train
-
 os.putenv("no_pipe", "1")
-global_config: DistributedTrainingConfig = DistributedTrainingConfig()
 
-
-@hydra.main(config_path="./conf", version_base=None)
-def load_config(conf) -> None:
-    global global_config
-    global_config = __load_config(
-        conf,
-        os.path.join(os.path.dirname(__file__), "conf", "global.yaml"),
-        import_libs=False,
-    )
-
+import method  # noqa: F401
+from cyy_naive_lib.log import redirect_stdout_to_logger
+from distributed_learning_simulation import load_config, train
 
 if __name__ == "__main__":
-    load_config()
-    train(config=global_config)
+    with redirect_stdout_to_logger():
+        config_path = os.path.join(os.path.dirname(__file__), "conf")
+        config = load_config(
+            config_path=config_path,
+            global_conf_path=os.path.join(config_path, "global.yaml"),
+        )
+        train(config=config, single_task=True)
