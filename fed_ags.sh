24ddb7d080b502f3e999aff94ff1cf78d283dbc9
Yuanyuan Chen <cyyever@outlook.com>
2025-12-18 12:44:00 +0800

    Add fed_ags

--- fed_ags.sh
+++ fed_ags.sh
@@ -1,4 +1,3 @@
-# fed_ags
-for configname in cs.yaml PubMed.yaml reddit.yaml yelp.yaml; do
-  python3 ./simulator.py --config-name fed_ags/${configname} ++fed_ags.worker_number=50 ++fed_ags.round=50 ++fed_ags.model_name=TwoGATCN ++fed_ags.epoch=1 ++fed_ags.dataloader_kwargs.batch_number=10 ++fed_ags.learning_rate=0.001 ++fed_ags.algorithm_kwargs.edge_drop_rate=0.99 ++fed_ags.fake_weight_decay=0.001 ++fed_ags.exp_name="fed_ags" ++fed_ags.algorithm_kwargs.min_sharing_interval=2
+for configname in cs.yaml PubMed.yaml yelp.yaml reddit.yaml ogbn-products.yaml; do
+  python3 ./simulator.py --config-name fed_ags/${configname} ++fed_ags.worker_number=100 ++fed_ags.exp_name="fed_ags" ++fed_ags.algorithm_kwargs.sample_percent=0.7 ++fed_ags.algorithm_kwargs.edge_drop_rate=0.5 ++fed_ags.algorithm_kwargs.max_epoch=50 ++fed_ags.algorithm_kwargs.warmup_rounds=1 ++fed_ags.algorithm_kwargs.total_epoch=500
 done
